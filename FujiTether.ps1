ab453311cc0a32eabe2aed6ed2d9a20d0bd436f7
Andreas Brett <andreasbrett@users.noreply.github.com>
2017-03-01 14:55:00 +0100

    initial release

---
+++ FujiTether.ps1
@@ -0,0 +1,102 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+#	Mode		Total	transfer	display		comment
+#	-------------------------------------------------------------
+#	USB2.0		7.58s	2.93s		4.65s		Lightroom CC
+#	USB3.0		6.76s	2.8s		3.8s		Lightroom CC
+#	USB2.0		2.9s	2.6s		0.3s		FujiTether
+#	USB3.0		2.7s	2.4s		0.3s		FujiTether
+
+
+# ---------------------------------
+# change these settings if you wish
+# ---------------------------------
+$FujiXAcquire = "C:\Program Files (x86)\FUJIFILM\XAcquire\XAcquire.exe"
+$watchExtension = "*.jpg"
+$moveExtension = "*.raf"
+$watchIntervalInMsec = 250
+
+
+Write-Host "FujiTether"
+Write-Host "-----------------------------------------------------------------"
+
+# retrieve Fuji X-Acquire destination folder from registry
+$watchFolder = (Get-ItemProperty -Path HKCU:\SOFTWARE\COM.FUJIFILM.DENJI\XAcquire\Preferences -Name Destination -ErrorAction SilentlyContinue).Destination
+$storeFolder = "$($watchFolder)\FujiTether"
+
+if (-Not $watchFolder) {
+    Write-host "ERROR: Fuji X-Acquire destination folder could not be read! Did you download and install it?"
+
+} else {
+
+    Write-Host " - watched folder: $($watchFolder)\$($watchExtension)"
+    Write-Host " - destination:    $($storeFolder)"
+    Write-Host " - interval:       $($watchIntervalInMsec)msec"
+    Write-Host ""
+    Write-Host " > Press Ctrl+C to stop"
+    Write-Host ""
+
+    # catch ctrl+c
+    [console]::TreatControlCAsInput = $true
+
+    # start Fuji X-Acquire upfront
+    & $FujiXAcquire
+
+    # create $storeFolder
+    New-Item -ItemType Directory -Force -Path "$($storeFolder)" | Out-Null
+
+    # move already existing pictures first
+    Get-ChildItem $watchFolder -Filter $watchExtension | ForEach-Object {
+        Move-Item $_.FullName -Destination $storeFolder
+    }
+
+    # infinite loop (ctrl+c to stop)
+    while($true) {
+
+	    # ctrl+c pressed?!
+	    if($Host.UI.RawUI.KeyAvailable -and (3 -eq [int]$Host.UI.RawUI.ReadKey("AllowCtrlC,IncludeKeyUp,NoEcho").Character)) {		
+		    Write-Host " * Ctrl+C detected. Quitting."
+			
+			# stop Fuji X-Acquire
+		    Stop-Process -Name XAcquire -ErrorAction SilentlyContinue
+		
+		    # break out of the loop
+		    break
+	    }
+    
+        # fetch all watched file types from watchFolder
+        if ($files = Get-ChildItem $watchFolder -Filter $watchExtension) {
+		    Write-Host " * $($files[0].Name)"
+		
+		    # move first file to subfolder
+		    Move-Item -Path $files[0].FullName -Destination $storeFolder
+		
+            # kill old viewer
+		    Stop-Process -Name Microsoft.Photos -ErrorAction SilentlyContinue
+		
+		    # wait a tiny bit for the move operation to be completed
+		    Start-Sleep -Milliseconds 350
+		
+		    # open viewer
+		    Invoke-Item -Path "$($storeFolder)\$($files[0].Name)"
+
+            # send viewer to fullscreen (via keypress F11)
+            Start-Sleep -Milliseconds 150
+            [System.Windows.Forms.SendKeys]::Sendwait("{F11}");
+        }
+	
+	    # move files matching $moveExtension
+	    Get-ChildItem $watchFolder -Filter $moveExtension | ForEach-Object {
+		    Move-Item $_.FullName -Destination $storeFolder
+	    }
+
+        # wait for next round
+        Start-Sleep -Milliseconds $watchIntervalInMsec
+    }
+}
+
+# wait for user to end program
+Write-Host ""
+Write-Host "-----------------------------------------------------------------"
+Write-Host "Press any key to close this window."
+$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # FujiTether
-Quickest way of shooting tethered with your Fuji X-T1, X-T2 and GFX50s (windows only)
+**Quickest** way of shooting tethered with your Fuji X-T1, X-T2 and GFX50s (**windows only**)
+
+## Prerequisites:
+- Windows 10
+- Fujifilm X-Acquire (download: http://www.fujifilm.com/support/digital_cameras/software/x_acquire/win/)
+
+## What about Lightroom's AutoImport Feature?
+While there is a big point in simply using Lightroom with its builtin feature **AutoImport** the goal of FujiTether is to display your photos **as quickly as possible**. If timing is of utmost importance and you can live with doing all the cataloging stuff *after* the photoshoot this script is for you.
+
+I analyzed the photo transfer of X-Acquire and it is really fast! What takes long when using Lightroom is all the processing that is done:
+
+- importing to the catalog
+- keywording the photo
+- setting metadata
+- rendering previews
+
+This is what I roughly measured with a stopwatch:
+- X-Acquire transfers a JPG in under ***3sec*** to your computer (not much slower when using USB 2.0)
+- Lightroom takes ***4sec*** to finally display the image
+- **FujiTether takes *0.3sec* to display the image**  
+
+## How to use it
+There's not much to do to get FujiTether running.
+
+If you've downloaded and installed **X-Acquire** from the Fujifilm website, configure it once to your liking. Run **FujiTether** by right-clicking the main file (***FujiTether.ps1***) and choosing **Run with Powershell**. FujiTether will figure out where files are uploaded to and poll this folder 4 times a second (=250msec) for new photos. New files are moved to a subfolder **FujiTether** and the latest JPEG will be displayed in fullscreen using Windows' onboard Photo Viewer in fullsreen mode.
+
+This is what makes FujiTether so fast and efficient: **high speed polling** and **instant display**.
+
+## Best X-Acquire Settings
+- I choose to transfer JPEG only if speed counts and/or I'm using a USB 2.0 cable
+- I choose to transfer JPEG+RAW if I want to work with all images right away after the photo shoot and/or I'm using a USB 3.0 cable (transfer of JPEG+RAW is not much slower than JPEG only)
+- I always save JPEG+RAW to my camera for backup reasons
+
+## Other Benefits?
+FujiTether is a 1-click solution setting up **Tethered Shooting** right away. If you run the tool everything else will run with it. No need to run X-Acquire and no need to setup anything else. Setting up a tethered photoshoot couldn't be quicker. 
+
+## Are there really no configurable options?!
+There are some variables that you can tweak within the source code.
+
+- **$FujiXAcquire**: specify path to Fuji X-Acquire executable (if you installed to a different folder)
+- **$watchIntervalInMsec**: specify the polling interval (250msec did not slow down my Surface Pro 4 ***at all***)
+- **$storeFolder**: specify a different folder to store final images in
